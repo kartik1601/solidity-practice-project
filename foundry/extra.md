803c3da4e728c6b680a231d6e54152632c28be41
Kartik Upadhyay <kartiku904@gmail.com>
2024-06-26 22:08:56 +0530

    Include env protection -Medium

---
+++ foundry/extra.md
@@ -0,0 +1,11 @@
+### CAST
+#### cast --to-base
+- converts the base of hexadecimal into other bases like decimal
+> cast --to-base 0x715b6 dec
+> 464310
+
+### TRANSACTION (.json)
+#### Missing attributes
+- r, s, v == private key signature, helps to sign the transaction
+#### Nonce
+- Important to figure out the transaction numbers for unique transactions
--- foundry/script/DeploySimpleStorage.s.sol
+++ foundry/script/DeploySimpleStorage.s.sol
@@ -7,5 +7,10 @@ import {SimpleStorage} from "../src/SimpleStorage.sol";
 contract DeploySimpleStorage is Script {
     function run() external returns(SimpleStorage){
         vm.startBroadcast(); // Special keyword from Script.sol 
+        /* TRANSACTIONS IN BETWEEN */
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        
+        return simpleStorage;
     }
 }
